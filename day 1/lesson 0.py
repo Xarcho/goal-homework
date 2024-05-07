337f9a565d751dcb780a1af35afd618527c021a7
Xarcho <xarcho1231@gmail.com>
2024-05-07 17:50:14 +0400

    first commit

---
+++ day 1/lesson 0.py
@@ -0,0 +1,38 @@
+from turtle import *
+
+speed(30)
+width (7)
+color("Purple")
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(70)
+color("Yellow")
+left(90)
+forward(120)
+right(90)
+forward(60)
+right(90)
+forward(120)
+
+penup()
+goto(200, 200)
+pendown()
+
+color("Red")
+begin_fill()
+right(150)
+forward(200)
+left(120)
+forward(200)
+end_fill()
+
